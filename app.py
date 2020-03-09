8925452ff41551e63df277e6e9e5dbcf16c1ce4c
Jose <jocvegar@gmail.com>
2020-03-08 19:31:55 -0600

    Removes print statement

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-print(os.environ['APP_SETTINGS'])
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,10 +11,6 @@ def index():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-    
-    
+     
 if __name__ == "__main__":
     app.run()
-
-
-
