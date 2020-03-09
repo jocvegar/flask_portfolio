e976058fe802b7f3e8f65f39700a8f0f014023f7
Jose <jocvegar@gmail.com>
2020-03-08 19:28:58 -0600

    loads several stuff

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+print(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+    
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+
+
