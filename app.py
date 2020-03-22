eeac51d2e382ca8ae27c7c658b6e2eadfae0a9c9
Jose <jocvegar@gmail.com>
2020-03-22 12:30:58 -0600

    Updates index  page

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def index():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-     
+    
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'ajdflkajfdlajfrandomasjdfcharactesr'
 
 class ProductionConfig(Config):
     DEBUG = False
